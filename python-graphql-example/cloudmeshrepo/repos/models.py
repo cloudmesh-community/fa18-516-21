31a215b9e5e1d98dd1220274d86fb68783ce9fdf
Vineet Barshikar <vbarshik@indiana.edu>
2018-09-19 20:49:30 -0400

    Moved the project under graphql-examples folder

--- python-graphql-example/cloudmeshrepo/repos/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Repo(models.Model):
-    url = models.URLField()
-    name = models.TextField(blank=False)
-    full_name = models.TextField(blank=False)
-    description = models.TextField(blank=True)
