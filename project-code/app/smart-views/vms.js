d4877a527117daf7cab548653dcf86c7acd93adb
vineetbarshikar <vineet.barshikar@hotmail.com>
2018-11-16 22:29:35 -0500

    Added mutation for changing the state of VM

--- project-code/app/smart-views/vms.js
+++ project-code/app/smart-views/vms.js
@@ -29,20 +29,59 @@ export default class VMs extends Backbone.View {
         }
     }
 
+    reload(frame, card, e) {
+        let vmkind = $(card).data('vmkind');
+        let vm = VMs.vmClause(vmkind);
+        let vmQuery = { "query" :"{ " + vm.clause + 
+                " { edges { node { host, name, region, publicIps, privateIps, image, state} } } }"};
+        
+        Api.post(vmQuery).then((res) => {
+            res.data[vm.clause].edges.forEach(e => {
+                e.node.isRunning = e.node.state.toLowerCase() === "running";
+                e.node.isStopped = e.node.state.toLowerCase() === "stopped";
+                e.node.isSuspended = e.node.state.toLowerCase() !== "running" && e.node.state.toLowerCase() !== "stopped";
+            });
+            frame.$el.html(vmTemplate({title: vm.title, edges: res.data[vm.clause].edges}));
+
+            $('.mdc-button').on('click', function(eb) {
+                let action = $(this).data('action');
+                let newState, vmMutation;
+                
+                if (vmkind === "aws") {
+                    if (action === "stop") {
+                        newState = "stopped";
+                    } else if (action === "shutdown") {
+                        newState = "terminated";
+                    } else {
+                        newState = "running";
+                    }
+                    vmMutation = { "query": "mutation { updateAws(host:\"" + $(this).data('host') + "\", state:\"" + newState + "\") { aws { state } } }" };
+                } else if (vmkind === "azure") {
+                    if (action === "stop") {
+                        newState = "Stopped";
+                    } else if (action === "shutdown") {
+                        newState = "Deallocated";
+                    } else {
+                        newState = "Running";
+                    }
+                    vmMutation = { "query": "mutation { updateAzure(host:\"" + $(this).data('host') + "\", state:\"" + newState + "\") { azure { state } } }" };
+                }
+
+                Api.post(vmMutation).then((res) => {
+                    if (res && res.data) frame.reload(frame, card, e);
+                });
+            });
+        });
+    }
+
     render() {
-        let parent = this.$el;
-        parent.html(selectTemplate());
+        let parent = this;
+        parent.$el.html(selectTemplate());
 
         $('.mdc-card').on("click", function(e) {
             e.preventDefault();
-
-            let vm = VMs.vmClause($(this).data('vmkind'));
-            let vmQuery = { "query" :"{ " + vm.clause + 
-                    " { edges { node { host, name, region, publicIps, privateIps, image, state} } } }"};
             
-            Api.post(vmQuery).then((res) => {
-                parent.html(vmTemplate({title: vm.title, edges: res.data[vm.clause].edges}));
-            });
+            parent.reload(parent, this, e);
         });
     }
 }
--- project-code/cloudmesh-graphql-server/schema.py
+++ project-code/cloudmesh-graphql-server/schema.py
@@ -19,4 +19,35 @@ class Query(graphene.ObjectType):
     allAwss = MongoengineConnectionField(AWS)
     allAzures = MongoengineConnectionField(Azure)
 
-schema = graphene.Schema(query=Query, types=[AWS, Azure])
+class UpdateAws(graphene.Mutation):
+    class Arguments:
+        host = graphene.String()
+        state = graphene.String()
+
+    aws = graphene.Field(AWS)
+
+    def mutate(self, info, host, state):
+        print(host)
+        aws = AwsModel.objects.get(host=host)
+        aws.state = state
+        aws.save()
+        return UpdateAws(aws=aws)
+
+class UpdateAzure(graphene.Mutation):
+    class Arguments:
+        host = graphene.String()
+        state = graphene.String()
+
+    azure = graphene.Field(Azure)
+
+    def mutate(self, info, host, state):
+        azure = AzureModel.objects.get(host=host)
+        azure.state = state
+        azure.save()
+        return UpdateAzure(azure=azure)
+
+class Mutation(graphene.ObjectType):
+    update_aws = UpdateAws.Field()
+    update_azure = UpdateAzure.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation, types=[AWS, Azure])
